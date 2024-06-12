c0922a9bfe83211de447e735dda815adbb3d7fb2
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-06-12 16:33:55 +0700

    feat: Add null check for RobotKey.keyPress

--- client/src/utils/RobotKey.java
+++ client/src/utils/RobotKey.java
@@ -2,6 +2,9 @@ package utils;
 
 public class RobotKey {
     public static void keyPress(int key) {
+        if (key == 0) {
+            return;
+        }
         try {
             java.awt.Robot robot = new java.awt.Robot();
             robot.keyPress(key);
